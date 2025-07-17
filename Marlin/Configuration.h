6f4df5e746badeb018189ea1f81c482006169bc1
Bukowa <gitbukowa@gmail.com>
2025-07-17 20:03:45 +0200

    change Z OFFSET to 0

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1664,7 +1664,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -21, -42, -1.5 }
+#define NOZZLE_TO_PROBE_OFFSET { -21, -42, 0 }
 
 // Enable and set to use a specific tool for probing. Disable to allow any tool.
 #define PROBING_TOOL 0
